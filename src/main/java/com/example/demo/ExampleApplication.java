d09e4f68ff592dfcf1bc5654428dbf619efb69c9
yuepeng <erupts@126.com>
2021-01-28 10:28:09 +0800

    增加war的打包代码

--- src/main/java/com/example/demo/ExampleApplication.java
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import xyz.erupt.core.annotation.EruptScan;
 
@@ -15,13 +17,19 @@ import java.net.URISyntaxException;
 @ComponentScan({"xyz.erupt", "com.example.demo"}) // ↓ xyz.erupt必须有
 @EntityScan({"xyz.erupt", "com.example.demo"})    // ↓ 如果包名com.example.demo有变化
 @EruptScan({"xyz.erupt", "com.example.demo"})     // → 要修改为变化后的包名
-public class ExampleApplication {
+public class ExampleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
         SpringApplication.run(ExampleApplication.class, args);
         System.setProperty("java.awt.headless", "false");
         Desktop.getDesktop().browse(new URI("http://localhost:8080"));
         System.err.println("详细使用方法，请阅读：README.md");
+    }
+
+    //打WAR包的配置
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExampleApplication.class);
     }
 
 }
