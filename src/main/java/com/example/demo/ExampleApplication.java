c2f4832271a43765081c7ce8e40092de9fabe501
yuepeng <erupts@126.com>
2021-07-18 01:58:18 +0800

    修复增加操作系统是否支持AWT校验，防止非图形化界面系统启动报错

--- src/main/java/com/example/demo/ExampleApplication.java
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import xyz.erupt.core.annotation.EruptScan;
 
 import java.awt.*;
@@ -19,10 +20,12 @@ public class ExampleApplication extends SpringBootServletInitializer {
 
     //详细使用方法详见项目内 README.md 文件说明
     public static void main(String[] args) throws URISyntaxException, IOException {
-        SpringApplication.run(ExampleApplication.class, args);
-        if (Desktop.isDesktopSupported()) {
+        ConfigurableApplicationContext context = SpringApplication.run(ExampleApplication.class, args);
+        try {
             System.setProperty("java.awt.headless", "false");
             Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+        } catch (Exception e) {
+            System.setProperty("java.awt.headless", "true");
         }
         System.err.println("详细使用方法，请阅读：README.md");
     }
