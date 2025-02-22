f2d461b95e9a72f720dcfc593782f9b37a008818
yuepeng <erupts@126.com>
2025-02-22 16:33:21 +0800

    upgrade to 1.12.19

--- src/main/java/com/example/demo/ExampleApplication.java
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -2,18 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import xyz.erupt.core.annotation.EruptScan;
 
 import java.awt.*;
 import java.net.URI;
 
 @SpringBootApplication
-@EntityScan
 @EruptScan
-public class ExampleApplication extends SpringBootServletInitializer {
+public class ExampleApplication {
 
     //详细使用方法详见项目内 README.md 文件说明
     public static void main(String[] args) {
@@ -24,12 +20,6 @@ public class ExampleApplication extends SpringBootServletInitializer {
         } catch (Exception ignore) {
         }
         System.err.println("详细使用方法，请阅读：README.md");
-    }
-
-    //打WAR包的配置
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ExampleApplication.class);
     }
 
 }
